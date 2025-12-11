7f900b3f11d30769a8e9e3878ba310020818850f
Sehrish <sehrishjaved119@gmail.com>
2025-12-11 14:27:29 +0500

    Completed task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,9 +18,18 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    // 1. Retrieve the ISBN from the request parameters (Hint)
+    const isbn = req.params.isbn;
+
+    // 2. Check if the ISBN exists in the imported 'books' object
+    if (books[isbn]) {
+        // 3. If found, return the book details
+        return res.status(200).json(books[isbn]);
+    } else {
+        // 4. If not found, return a 404 error
+        return res.status(404).json({message: `Book with ISBN ${isbn} not found.`});
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
